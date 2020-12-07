4ae96801ae037d22e3c4fb315e8820ec7c3581ae
Yme Bogfjelldal <ymebogfjelldal@Ymes-MacBook-Air.local>
2020-12-07 18:52:36 +0100

    House hunting app for my finals

---
+++ app/src/main/java/com/example/homehunting/Housing.kt
@@ -0,0 +1,22 @@
+package com.example.homehunting
+
+class Housing(var address: String, var type: Type, var price: Int, var ownerForm: String, var amenities: String) {
+}
+
+enum class Type() {
+        COTTAGE,
+        FLAT,
+        HOUSE,
+        PENTHOUSE,
+        ROOM,
+        FARM,
+        TREEHOUSE,
+        BOX
+    }
+
+enum class OwnerForm (){
+        RENT,
+        SELFOWN,
+
+
+}
---
+++ app/src/main/java/com/example/homehunting/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.homehunting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forestCottage = Housing( "Inside the forest in a clearing in the trees", Type.COTTAGE,
+                                      600000, "Self own",
+                                    "On property well, herbal garden & shrine in garden for hedonistic Gods.")
+
+        val hauntedPenthouse = Housing("Friday street nr. 13", Type.PENTHOUSE,
+                                        10000000, "Self own",
+                                    "Creaky floors, possessed super, haunted couch and pet friendly.")
+
+        val vampireRoom = Housing("Castle road nr. 1666", Type.ROOM,
+                                    8000, "Rent",
+                                "Fully equipped kitchen, bathroom, washroom & sexy vampire roommates.")
+
+        val cityFlat = Housing("Courier road nr. 7E", Type.FLAT,
+                                10000, "Rent",
+                            "Washing cellar, extra storage in attic, quiet neighbours and close to subway.")
+
+        val cardboardBox = Housing("Under bridge by the park", Type.BOX,
+                                    500, "Self own",
+                                "Right on the river, walking distance to the park, no noisy neighbours.")
+
+        val list = mutableListOf<Housing>(forestCottage, hauntedPenthouse, vampireRoom, cityFlat, cardboardBox)
+    }
+
+    fun main (){
+
+        for () {
+
+            
+
+        }
+
+    }
+}
